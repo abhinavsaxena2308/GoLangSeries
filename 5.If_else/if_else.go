363332cf2716ecbdbd19fdd1b3970ffee88cb6b7
Abhinav Saxena <abhinav.exam.2003@gmail.com>
2026-02-27 12:34:19 +0530

    code added

--- 5.If_else/if_else.go
+++ 5.If_else/if_else.go
@@ -1,10 +1,32 @@
 package main
 
-func main(){
-	i:=12
-	if i<10{
+import "fmt"
+
+func main() {
+	i := 12
+	if i < 10 {
 		println("number is less than 10")
-	} else{
+	} else {
 		println("number is greater than 10")
 	}
+
+	//logical opn
+	var role = "user"
+	var hasPermissions = false
+
+	if role == "admin" || hasPermissions {
+		fmt.Println("admin verified")
+	} else {
+		println("admin not verified")
+	}
+
+	//shorter method
+	// we can declare a variable inside if construct
+	if age:=15; age>=18{
+		fmt.Println("person is adult")
+	}else{
+		fmt.Println("person is an adult")
+	}	
+
+	// go does not have ternary , you will have to use normal if else
 }
